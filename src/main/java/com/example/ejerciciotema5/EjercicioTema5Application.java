f793415fc7d1b3ca39612e8bf4922c54fe128e5d
ElenaMegia <emegican@myuax.com>
2023-02-20 15:49:26 +0100

    Initial commit

---
+++ src/main/java/com/example/ejerciciotema5/EjercicioTema5Application.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciotema5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioTema5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioTema5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ejerciciotema5/EjercicioTema5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejerciciotema5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioTema5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
