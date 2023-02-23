c5f23c39c0e282a5d86dd038da831f8bf615eea5
ElenaMegia <emegican@myuax.com>
2023-02-23 17:24:46 +0100

    Ejercicio 5

--- src/main/java/com/example/ejerciciotema5/Ejercicio5.java
+++ src/main/java/com/example/ejerciciotema5/Ejercicio5.java
@@ -1,4 +1,24 @@
 package com.example.ejerciciotema5;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
+    //Escriba un algoritmo que pida al usuario una palabra sin acento, en minúsculas y seguida de un carácter en particular para indicar el final (# por ejemplo), y que le diga al usuario si esta palabra es un palíndromo.
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String palabra = sc.nextLine();
+        boolean esPalindromo = true;
+        for (int i = 0; i < palabra.length() / 2; i++) {
+            if (palabra.charAt(i) != palabra.charAt(palabra.length() - 1 - i)) {
+                esPalindromo = false;
+                break;
+            }
+        }
+        if (esPalindromo) {
+            System.out.println("Es palíndromo");
+        } else {
+            System.out.println("No es palíndromo");
+        }
+    }
 }
