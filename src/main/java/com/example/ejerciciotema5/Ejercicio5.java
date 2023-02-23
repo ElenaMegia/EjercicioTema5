d0843116eed15364c823ae067d8919b12a168a07
ElenaMegia <emegican@myuax.com>
2023-02-23 17:31:49 +0100

    Ejercicio 5 terminado con excepciones necesarias

--- src/main/java/com/example/ejerciciotema5/Ejercicio5.java
+++ src/main/java/com/example/ejerciciotema5/Ejercicio5.java
@@ -7,18 +7,31 @@ public class Ejercicio5 {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        System.out.println("Introduzca una palabra:");
         String palabra = sc.nextLine();
-        boolean esPalindromo = true;
-        for (int i = 0; i < palabra.length() / 2; i++) {
-            if (palabra.charAt(i) != palabra.charAt(palabra.length() - 1 - i)) {
-                esPalindromo = false;
-                break;
+        if(palabra.charAt(palabra.length()-1) != '#'){
+            System.out.println("La palabra no termina en #");
+
+        }else {
+            boolean esPalindromo = true;
+            //crear una variable String que contenga la palabra sin el último carácter
+            palabra = palabra.substring(0, palabra.length() - 1);
+            for (int i = 0; i < palabra.length()-1 / 2; i++) {
+                if (palabra.charAt(i) != palabra.charAt(palabra.length() - 1 - i)) {
+                    esPalindromo = false;
+                    break;
+                }else if(palabra.length()-1==0){
+                    esPalindromo = false;
+                    System.out.println("La palabra no tiene más de un carácter");
+                    break;
+                }
             }
-        }
-        if (esPalindromo) {
-            System.out.println("Es palíndromo");
-        } else {
-            System.out.println("No es palíndromo");
+            if (esPalindromo) {
+                System.out.println("Es palíndromo");
+            } else {
+                System.out.println("No es palíndromo");
+            }
         }
     }
 }
